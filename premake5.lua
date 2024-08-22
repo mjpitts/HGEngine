5059af192da71a4d36cb68308c4a8f45ac9c4367
Michael Pitts <michaeljeffreypitts@gmail.com>
2024-08-21 17:29:43 -0700

    Commit before attempting premake again.

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "HGEngine"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "HGEngine"
+	location "HGEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name/src/**.h",
+		"%{prj.name/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"HGE_PLATFORM_WINDOWS", 
+			"HGE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines"HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines"HZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines"HZ_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "HGEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name/src/**.h",
+		"%{prj.name/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"HGEngine/vendor/spdlog/include",
+		"HGEngine/src"
+	}
+
+	links
+	{
+		"HGEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"HGE_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines"HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines"HZ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines"HZ_DIST"
+		optimize "On"
